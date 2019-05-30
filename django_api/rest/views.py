4d11186f06215ff98413351293c2d834d5ba4285
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-30 13:15:07 +0300

    Added rest_framework dependecies.

--- django_api/rest/views.py
+++ django_api/rest/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import employees
+from . serializers import employeeserializer
