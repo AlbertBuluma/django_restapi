871413367f414703f472307dfe9d8e2aed064c32
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-30 12:46:43 +0300

    Modified INSTALLED_APPS to add project app.

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_api',
+    'rest',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
