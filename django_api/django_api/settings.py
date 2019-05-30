8098c9eefb5941ff4c80138b52ed67cdbd6d89a1
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-30 16:54:18 +0300

    Modifying DB dictionary to integrate a postgres DB.

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', #Switching to Postgres DB
+        'NAME': 'albertdb',
+        'USER': 'albert',
+        'PASSWORD': 'albert',
+        'HOST':'localhost',
+        'PORT':'5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
