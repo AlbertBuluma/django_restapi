4195a2d715efd9ef72d3a59e8f8d8d32b9ecdc2e
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-30 15:36:46 +0300

    Modified views to ModelViewset with more CRUD functionality.

--- django_api/rest/views.py
+++ django_api/rest/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
-# Create your views here.
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import render
+from rest_framework import viewsets
 from . models import employees
-from . serializers import employeeSerializer
+from .serializers import employeeSerializer
 
-
-class EmployeeList(APIView):
-
-    def get(self, request):
-        employeesl = employees.objects.all()
-        serializer = employeeSerializer(employeesl, many=True)
-        return Response(serializer.data)
-
-
-    def post(self):
-        pass
+class EmployeeView(viewsets.ModelViewSet):
+    queryset = employees.objects.all()
+    serializer_class = employeeSerializer 
