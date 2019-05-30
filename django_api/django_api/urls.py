cf1533617bd67448af5196e5ac0704791c186b5e
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-30 14:02:59 +0300

    Added rest_framework dependenices and employees endpoint.

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employees/', views.EmployeeList.as_view()),
 ]
