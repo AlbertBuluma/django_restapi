0bb6a5520a37bb1ee477a56dc78dc942c99f8911
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-30 13:13:07 +0300

    Removing emp_id field.  -m "id field is created by default"

--- django_api/rest/models.py
+++ django_api/rest/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class employees(models.Model):
     firstname = models.CharField(max_length=15)
     lastname = models.CharField(max_length=15)
-    emp_id = models.IntegerField
+    # emp_id = models.IntegerField()
 
     def __str__(self):
         return self.firstname
