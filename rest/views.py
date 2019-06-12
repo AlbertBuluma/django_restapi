53a0ba575414da299019d6e8ea7ce3b618f3723d
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-12 11:07:14 +0300

    imported permissions class from rest_framework.

--- rest/views.py
+++ rest/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from . models import Employees
 from .serializers import employeeSerializer
 
 class EmployeeView(viewsets.ModelViewSet):
     queryset = Employees.objects.all()
     serializer_class = employeeSerializer 
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
