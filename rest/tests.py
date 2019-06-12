25118e40129d9ecafd2d90fca07b978ad83766d1
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-12 09:46:06 +0300

    Modified tests.

--- rest/tests.py
+++ rest/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import include, path, reverse
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework.test import APITestCase, APIClient, URLPatternsTestCase
 from rest_framework import routers
 from rest.models import Employees
 from . import views
@@ -14,32 +14,37 @@ urlpatterns = [
 ]
 
 class EmployeeTestCase(TestCase):
-
-    # def createEmployee(self):
-    #     employee = Employees.object.create(firstname = 'Albert', lastname = 'Buluma')
-    #     return employee
-
-    #Testing the Employees model
-    # def test_Employees_creation(self):
-    #     created = self.createEmployee()
-        # self.assertTrue(isinstance(created, Employees))
-    #     self.assertEqual(created.__str__(), 'Albert')
-    #     self.assertEqual(created.lastname, 'Buluma')
-
+    
+    client = APIClient()
+    client.post('/<int:id>', {firstname:'Jerry'}, format='json')
+    # Testing Employee creation
     def test_Employee_names(self): 
         employee = Employees.objects.create(firstname='Albert', lastname='Buluma')
         employee.save()
         self.assertTrue(isinstance(employee, Employees))
         self.assertEqual(employee.firstname,'Albert')
         self.assertEqual(employee.lastname, 'Buluma')
-    # #testing the home view
+   
+    # Testing the API root view
+    def test_Employee_list(self):
+        url = reverse('api-root')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(len(response.data), 1)
 
-    # def test_view_home(self):
-    #     created = self.createUser()
-    #     url = reverse('list-all')
-    #     resp=self.client.get(url)
-    #     self.assertEqual(resp.status_code, 200)
-    #     self.assertIn(created.fname, 'Faisal')
+    def test_Employee_update(self):
+        employee2 = Employees.objects.filter(id=1)
+        employee2.firstname='Van Dijk'
+        # self.assertTrue(isinstance(employee2, Employees))
+        self.assertEqual(employee2.firstname,'Van Dijk')
+
+    # def test_Employee_id(self):
+    #     count = Employees.objects.count()
+    #     url = reverse('api-root/<int:id>',kwargs={id:Employees.object.get(id=1)})
+    #     response = self.client.get(url, format='json')
+    #     self.assertEqual(response.status_code, 200)
+    
+
 
     # def test_update_post(self):
     #     post = Posts.objects.create(id=5, fname='Namayanja', sname='Masitula', sex='female', amount='9000000')
@@ -50,23 +55,3 @@ class EmployeeTestCase(TestCase):
     #     self.assertEqual(response.status_code, 302)
     #     post.refresh_from_db()
     #     self.assertEqual(post.fname, 'Namayanja')
-
-
-
-
-    # def test_Employee_names(self): 
-    #     employee = Employees.objects.create(firstname='Albert', lastname='Albert')
-    #     #employee.save()
-    #     self.assertEqual(employee.firstname,'Albert')
-
-    # def test_Employee_list(self):
-    #     url = reverse('api-root')
-    #     response = self.client.get(url, format='json')
-    #     self.assertEqual(response.status_code, 200)
-        # self.assertEqual(len(response.data), 1)
-
-    # def test_Employee_id(self):
-    #     count = Employees.objects.count()
-    #     url = reverse('api-root/<int:id>',kwargs={id:Employees.object.get(id)})
-    #     response = self.client.get(url, format='json')
-    #     self.assertEqual(response.status_code, 200)
